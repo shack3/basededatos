bbc54c5b302ba58338ff79795b06edffa7e8550b
CDenus <94920187+CDenus@users.noreply.github.com>
2021-11-24 17:55:46 +0100

    v 2.0
    Con el count mejor que no el not exists

--- Consultas/Consulta_H.sql
+++ Consultas/Consulta_H.sql
@@ -2,15 +2,15 @@
 #comprado todas las dagas disponibles.
 
 SELECT personaje.Nivel_personaje, personaje.Nombre_personaje
-FROM personaje, rol, compra
-WHERE rol.Tipo_rol = personaje.Tipo_rol
-AND personaje.Nombre_personaje = compra.Nombre_personaje
-AND rol.Tipo_rol = "MAGO"
-AND personaje.Nombre_personaje IN (SELECT personaje.Nombre_personaje
-									FROM personaje, mision_indi, enemigo
-									WHERE personaje.Nivel_personaje = mision_indi.Nombre_personaje
-									AND mision_indi.Cod_misionindi = enemigo.Cod_misionindi
-                                    GROUP BY Nombre_personaje
-                                    HAVING count(Nombre_enemigo) = (SELECT count(DISTINCT Nombre_enemigo) FROM enemigo))
+FROM personaje INNER JOIN rol ON rol.Tipo_rol = personaje.Tipo_rol
+			   INNER JOIN compra ON personaje.Nombre_personaje = compra.Nombre_personaje
+WHERE rol.Tipo_rol = "MAGO"
+AND personaje.Nombre_personaje IN(SELECT personaje.Nombre_personaje 
+								  FROM personaje INNER JOIN compra ON personaje.Nombre_personaje = compra.Nombre_personaje
+												 INNER JOIN daga ON compra.Id_daga = daga.Id_daga
+								  GROUP BY personaje.Nombre_personaje
+								  HAVING count(DISTINCT daga.Nombre_daga) = (SELECT count(DISTINCT daga.Nombre_daga) FROM daga))
 GROUP BY personaje.Nombre_personaje
-HAVING count(*) = (SELECT count(DISTINCT daga.Nombre_daga) FROM daga);
+HAVING COUNT(*) = (SELECT COUNT(DISTINCT enemigo.Nombre_enemigo)
+				   FROM enemigo
+                   WHERE enemigo.Nombre_tipoene = "ESPECTRO");
