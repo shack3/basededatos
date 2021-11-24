21ecac0553be0d3ca4fff530a56d5e997020f76c
CDenus <94920187+CDenus@users.noreply.github.com>
2021-11-24 17:30:05 +0100

    v 2.0
    Hecho con count mejor que con el not exists

--- Consultas/Consulta_G.sql
+++ Consultas/Consulta_G.sql
@@ -2,11 +2,8 @@
 #todos los druidas existentes. => Guerreros tales que no hay druida que no les haya dado pocion
 
 SELECT personaje.Nombre_personaje, rol.Vida_personaje, rol.Fuerza, personaje.Nombre_jugador
-FROM personaje, rol
+FROM personaje, rol, NPC_Regala
 WHERE personaje.Tipo_rol = rol.Tipo_rol
-AND rol.Tipo_rol = "GUERRERO"
-AND NOT EXISTS (SELECT * FROM npc_regala
-				WHERE NOT EXISTS(SELECT * FROM druida
-									 WHERE personaje.Nombre_personaje = npc_regala.Nombre_personaje
-									 AND npc_regala.ID_NPC = druida.ID_NPC));
-                                     
+AND personaje.Nombre_personaje = NPC_Regala.Nombre_personaje
+GROUP BY personaje.Nombre_personaje
+HAVING COUNT(*) = (SELECT count(DISTINCT ID_NPC) FROM druida);
