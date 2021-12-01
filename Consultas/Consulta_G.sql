4e16e04b01fcd811796e25447f37929c453637f1
CDenus <94920187+CDenus@users.noreply.github.com>
2021-12-01 16:01:34 +0100

    Delete Consulta_G.sql

--- Consultas/Consulta_G.sql
+++
@@ -1,9 +0,0 @@
-#Obtener nombres, vida y fuerza de guerreros que hayan recibido una poción de
-#todos los druidas existentes. => Guerreros tales que no hay druida que no les haya dado pocion
-
-SELECT personaje.Nombre_personaje, rol.Vida_personaje, rol.Fuerza, personaje.Nombre_jugador
-FROM personaje, rol, NPC_Regala
-WHERE personaje.Tipo_rol = rol.Tipo_rol
-AND personaje.Nombre_personaje = NPC_Regala.Nombre_personaje
-GROUP BY personaje.Nombre_personaje
-HAVING COUNT(*) = (SELECT count(DISTINCT ID_NPC) FROM druida);
